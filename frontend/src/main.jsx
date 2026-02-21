617ed837e1d2017c70fda6d744fc4bed595b3763
Adhiraj Singh <162025361+ADHIRAJ00000@users.noreply.github.com>
2026-02-22 03:41:45 +0530

    Wrap App component with SocketContextProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <PostContextProvider>
         <ChatContextProvider>
           <SocketContextProvider>
+            
             <App />
           </SocketContextProvider>
         </ChatContextProvider>
